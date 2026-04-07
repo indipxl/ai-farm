bba967b28ded42e4f1d0eca6bb357e90b516ebb7
nuraziilahawg-dev <nuraziilahawg@gmail.com>
2026-04-08 01:49:32 +0800

    fixes: update main.py

--- backend/main.py
+++ backend/main.py
@@ -21,8 +21,13 @@ load_dotenv()
 project_id = os.getenv("FIREBASE_PROJECT_ID")
 service_account_path = "gaia-sabah-c3-group2-aifarm-02d42c9eeb6a.json"
 
-cred = credentials.Certificate(service_account_path)
-firebase_admin.initialize_app(cred)
+if os.path.exists(service_account_path):
+    # runs on your local/cloud shell
+    cred = credentials.Certificate(service_account_path)
+    firebase_admin.initialize_app(cred)
+else:
+    # runs on Cloud Run
+    firebase_admin.initialize_app()
 db = firestore.client()
 
 # batches
@@ -33,6 +38,6 @@ app.include_router(batches_router, prefix="/api")
 from services.crops import router as crops_router
 app.include_router(crops_router, prefix="/api")
 
-if __name__ == "__main__":
-    port = int(os.getenv('PORT', 8080))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     port = int(os.getenv('PORT', 8080))
+#     uvicorn.run(app, host="0.0.0.0", port=port)
